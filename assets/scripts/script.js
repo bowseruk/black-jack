5ac18cf06468d8a6bad0c4f6cccf345b854f4fa7
bowseruk <13262235+bowseruk@users.noreply.github.com>
2023-03-20 16:36:28 +0000

    Added some temporary text to the page.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,22 +1,70 @@
-// Random Number Generator
-function generateNumber(min, max) {
-    // Times the random generator by the range of values needed and add the minimum
-    return Math.floor(Math.random() * (max - min) + min)
+class Card {
+    constructor(card, suit) {
+        this.card = card;
+    }
+    set card(number) {
+        switch(number){
+        case 2: case 3: case 4: case 5: case 6: case 7: case 8: case 9: case 10:
+            this.name = number;
+            this.value = number;
+            break;
+        case 11:
+            this.name = "Jack"
+            this.value = 10;
+            break;
+        case 12:
+            this.name = "Queen"
+            this.value = 10;
+            break;
+        case 13:
+            this.name = "King"
+            this.value = 10;
+            break;
+        case 1: case 14:
+            this.name = "Ace"
+            this.value = 11;
+    }
 }
 
-// Random Card Generator
-function drawCard() {
-    // The card has higher probability of being ten due to the number of face cards
-    card = generateNumber(2, 14);
-    // If the card is a face card, change the value to 10
-    if (card >= 11 && card < 14) {
-        card = 10;
-    } else if (card == 14) {
-        // Change the ace to an 11 value
-        card = 11;
-    }
-    return card;
 }
+// This is the deck
+var deck = [];
+// These are the suites we are playing with
+var suits = ["Clubs", "Diamonds", "Hearts", "Spades", ]
+// Deck Generator
+function deckGenerator(numberOfPacks, suits) {
+    for (i = 0; i < numberOfPacks; i++) {
+        for (j = 2; j < 15; j++) {
+            let card = [];
+            switch (j) {
+                case 2: case 3: case 4: case 5: case 6: case 7: case 8: case 9: case 10:
+                    card = [`${j}`, j];
+                    break;
+                case 11:
+                    card = ["Jack", 10];
+                    break;
+                case 12:
+                    card = ["Queen", 10];
+                    break;
+                case 13:
+                    card = ["King", 10];
+                    break;
+                case 14:
+                    card = ["Ace", 11];
+            };
+            for (k = 0; k < suits.length; k++) {
+                let input = [suits[k], card[0], card[1]];
+                deck.push(input);
+            };
+        };
+    };
+};
+
+// CSM Shuufle -- continous shuffle - 
+
+// Hand Shuffle -- Shuffle at set distance through shoe
+
+// ASM Shuffle -- shuffle at end of shoe
 
 function game() {
     // Player draws a card
@@ -60,4 +108,6 @@ function game() {
     }
 }
 
-game()
+deckGenerator(4, suits)
+console.log(deck)
+// game()
